083e5e92082361b17c6da148ece1c65733ffc2c0
Keruone <1723929976@qq.com>
2025-12-26 23:25:45 +0800

    feat(all): implement computational graph visualization
    Add computational graph visualization capability that supports generating DOT format files and
    rendering to PNG images via Graphviz. Includes style customization for Variable/Function nodes,
    recursive traversal logic for computation graphs, and fixes DOT syntax errors (single
    quotes/spelling issues).Visualization images can be generated with one click via plot_dot_graph
    function.

--- step/step24.py
+++ step/step24.py
@@ -4,6 +4,7 @@ if '__file__' in globals():
 
 import numpy as np
 from dezero import Variable
+from dezero.utils import plot_dot_graph
 
 def sphere(x, y):
 	z = x ** 2 + y ** 2
@@ -36,4 +37,8 @@ x = Variable(np.array(1.0))
 y = Variable(np.array(1.0))
 z = goldstein(x,y)
 z.backward()
+x.name = 'x'
+y.name = 'y'
+z.name = 'z'
 print(x.grad, y.grad)
+plot_dot_graph(z, verbose=False, to_file='goldstein.png')
